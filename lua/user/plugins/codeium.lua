365b09e089897a0807874eb37de6411f22d8ca2f
Kyle Qing <kyle.qing@hichiptech.com>
2024-03-25 12:04:32 +0000

    update

--- lua/user/plugins/codeium.lua
+++ lua/user/plugins/codeium.lua
@@ -1,9 +1,9 @@
 return {
 	-- add symbols-outline
 	["Exafunction/codeium.vim"] = {
-	 	 lazy = true,
-         event = "BufEnter",
-		 dependencies = {
+		lazy = true,
+		event = "BufEnter",
+		dependencies = {
 			"nvim-lua/plenary.nvim",
 			"hrsh7th/nvim-cmp",
 		},
